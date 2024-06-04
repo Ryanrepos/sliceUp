647dcc633a3e84b498eacfdee2a02243ed2b33cd
ryanrepos <mamarasulov911@gmail.com>
2024-06-04 13:58:38 +0900

    BRR: Initial setup

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+console.log("natija");
+
+
+
+import moment from "moment";
+
+const currentTime = moment().format("YYYY MM DD");
+console.log(currentTime);
+
+const person: string = "Ryan";
+const count: number = 100;
