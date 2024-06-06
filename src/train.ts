cfadb52a184838b85ac25af0065d81a40ac7e1d0
ryanrepos <mamarasulov911@gmail.com>
2024-06-06 20:12:45 +0900

    Task - H solved successfully

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,13 @@
-console.log("heello");
+function getPositiveNumbers(a: number[]): string {
+    const result: number[] = [];
+    for(const i of a) {
+        if(i > 0) {
+            result.push(i);
+        }
+    }
+    return result.join("")
+}
+
+console.log(getPositiveNumbers([1, 9, 5, -2, -8]));  // buyerda 195 raqamlarini qaytarib beryabdi. 
+
+// RUN: npm run train
