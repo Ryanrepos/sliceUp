6ea64dadf862e39c8609bb028ac1375768c8aa0e
ryanrepos <mamarasulov911@gmail.com>
2024-06-06 19:03:15 +0900

    feat: connect mongoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,19 @@
 // Architectural pattern - MVC, Dependency Injection, MVP
 // Butun proyektni suyagi
 
-
-
 // Desing pattern - Middleware, decorator
 // Prooyektning bir bo'lagining suyagi: masalan qo'ligi
 
 
+// MONGO DB:
+// CLUSTER => DATABASE => COLLECTION => DOCUMENT
+
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
-// MONGO DB:
-// CLUSTER => DATABASE => COLLECTION => DOCUMENT
+mongoose.connect(process.env.MONGO_URL as string, {}).then((data) => {
+    console.log("MongoDb connection is succeed");
+    const PORT = process.env.PORT ?? 3003;
+}).catch((err) => console.log("Error on connecting to MongoDB", err));
