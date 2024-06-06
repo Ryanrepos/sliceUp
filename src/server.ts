cce76378fa101622b9d88d858155ee0b2ae79075
ryanrepos <mamarasulov911@gmail.com>
2024-06-06 16:46:07 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -5,3 +5,12 @@
 
 // Desing pattern - Middleware, decorator
 // Prooyektning bir bo'lagining suyagi: masalan qo'ligi
+
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+console.log("PORT:", process.env.PORT);
+
+console.log("MONGO_URL:", process.env.MONGO_URL);
