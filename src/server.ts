064cf4915870b4b1b17ccca309a873573d891bb0
ryanrepos <mamarasulov911@gmail.com>
2024-06-06 17:01:55 +0900

    feat: create train area

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,5 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log("PORT:", process.env.PORT);
-
-console.log("MONGO_URL:", process.env.MONGO_URL);
+// MONGO DB:
+// CLUSTER => DATABASE => COLLECTION => DOCUMENT
