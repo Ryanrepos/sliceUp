921d49deb62dc1b6d76e80af29372970f62d2455
ryanrepos <mamarasulov911@gmail.com>
2024-06-30 06:33:44 +0900

    feat: signup image frontend validation completed

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -3,6 +3,31 @@ console.log("Signup frontend javascript file");
 $(function () {
     // jQuery methods go here...
 
+    const fileTarget = $(".file-box .upload-hidden");
+    let fileName;
+
+    fileTarget.on("change", function () {
+        if (window.FileReader) {
+            const uploadFile = $(this)[0].files[0];
+            console.log("uploadFile:", uploadFile);
+            const fileType = uploadFile["type"];
+
+            // allowing image types
+            const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+
+            if (!validImageType.includes(fileType)) {
+                alert("Please, insert only jpg, jpeg and png!")
+            } else {
+                if (uploadFile) {
+                    console.log(URL.createObjectURL(uploadFile));
+                    $(".upload-img-frame").attr("src", URL.createObjectURL(uploadFile)).addClass("success");
+                }
+                fileName = $(this)[0].files[0].name;
+            }
+            // image ni nomini kiritadi..
+            $(this).siblings(".upload-name").val(fileName);
+        }
+    });
 
 });
 
@@ -20,6 +45,14 @@ function validateSignupForm() {
 
     if (memberPassword !== confirmPassword) {
         alert("Password differs, please check!");
+        return false;
+    }
+
+    // Image validation..
+    const memberImage = $(".member-image").get(0).files[0] ? $(".member-image").ge(0).files[0].name : null;
+
+    if (!memberImage) {
+        console.log("Please insert restaurant image");
         return false;
     }
 
