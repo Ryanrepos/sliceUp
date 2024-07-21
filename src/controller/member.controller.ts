9a1832efab22ca0d3abab22ac6b2a4782d80f381
ryanrepos <mamarasulov911@gmail.com>
2024-07-21 13:56:28 +0900

    feat: develop authentication via token with cookies

--- src/controller/member.controller.ts
+++ src/controller/member.controller.ts
@@ -4,9 +4,9 @@ import {T} from "../libs/types/common";
 
 import MemberService from "../model/Member.service";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
-import Errors from "../libs/Errors";
+import Errors, { HttpCode } from "../libs/Errors";
 import AuthService from "../model/Auth.service";
-import { token } from "morgan";
+import { AUTH_TIMER } from "../libs/config";
 
 // Service modeldan instance olyabmiz
 const memberService = new MemberService();
@@ -19,14 +19,24 @@ memberController.signup = async (req: Request, res: Response) => {
     try {
         console.log("signup");
         const input: MemberInput = req.body;
+
+        // Call MemberService to sign up a new member
         const result = await memberService.signup(input);
+
+        // Generate a token for the signed up member
         const token = await authService.createToken(result)
-        console.log("token:", token);
-        
-        // TOKENS AUTHENTICATION
-        res.json({member: result});
+
+        // Cookie Timer
+        res.cookie("accessToken", token, { maxAge: AUTH_TIMER * 3600 * 1000,
+            httpOnly: false,
+            });
+            
+        // Respond with the signed up member and token
+        res.status(HttpCode.CREATED).json({ member: result, accessToken: token });
     } catch(err) {
         console.log("Error, signup", err);
+
+        // Handle specific errors or respond with a standard error
         if (err instanceof Errors) res.status(err.code).json(err);
         else res.status(Errors.standart.code).json(Errors.standart);
         // res.send(err);
@@ -38,14 +48,20 @@ memberController.login = async (req: Request, res: Response) => {
     try {
         console.log("login");
         const input: LoginInput = req.body;
-        const result = await memberService.login(input);
-        const token = await authService.createToken(result);
-        console.log("token:", token);
-        
-        //TOKENS AUTHENTICATION
 
-        console.log("result:", result);
-        res.send(result);
+        // Call MemberService to authenticate the login
+        const result = await memberService.login(input);
+
+        // Generate a token for the logged-in member
+        const token = await authService.createToken(result);
+
+        // Cookie Timer
+        res.cookie("accessToken", token, { maxAge: AUTH_TIMER * 3600 * 1000,
+        httpOnly: false,
+        });
+        
+        // Respond with the logged-in member and token
+        res.status(HttpCode.OK).json({ member: result, accessToken: token });
     } catch(err) {
         console.log("Error, processLogin:", err)
         if (err instanceof Errors) res.status(err.code).json(err);
